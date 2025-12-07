ccda971df5673f998596f9e207ad31bddc3f47c9
carlosaprost <carlosaprost@gmail.com>
2025-12-07 01:31:20 -0300

    angular 21

--- presupuesto-familiar/src/app/app.component.ts
+++ presupuesto-familiar/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 @Component({
     selector: 'app-root',
-    imports: [CommonModule, RouterOutlet, HttpClientModule],
+    imports: [RouterOutlet, HttpClientModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
